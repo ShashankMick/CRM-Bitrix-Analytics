b4fcaf6af5f5f77c8b635efcad70a4abe1edac13
ShashankMick <shashank.mick@gmail.com>
2024-10-24 00:08:07 +0530

    Update Bitrix_CRM_Analysis.py

--- Bitrix_CRM_Analysis.py
+++ Bitrix_CRM_Analysis.py
@@ -43,7 +43,7 @@ stages = ['Reach','Attract','Develop','Meeting Booked','SQL','Proposal','Contrac
 #stages
 
 # Making cumulative of stages by repeating all stages after it to that stage in an expanded dataframe
-@st.cache_data
+# @st.cache_data
 def expand_cumulative_stages(df, stages, stage_col='Stage'):
     expanded_data = []
     
