921daac6ab25e5d5840037c072f7a307452341e7
ShashankMick <shashank.mick@gmail.com>
2024-11-09 13:39:04 +0530

    Update Bitrix_CRM_Analysis.py

--- Bitrix_CRM_Analysis.py
+++ Bitrix_CRM_Analysis.py
@@ -78,106 +78,113 @@ def expand_cumulative_stages(df, stages, stage_col='Stage'):
 # Apply the transformation to create cumulative stages
 cum_stages_breakdown_expanded = expand_cumulative_stages(df, stages)
 cum_stages_breakdown_expanded['Count']=1
+# Only keep rows that appear once (no duplicates at all) - non_cumulative
+non_cum_stages_breakdown_expanded = cum_stages_breakdown_expanded.drop_duplicates(keep=False)
 # Sidebar for user inputs
 st.sidebar.title("Expedify Bitrix CRM Analytics")
 
-# Add date range filter in the sidebar
-min_date = cum_stages_breakdown_expanded['Created'].min()
-max_date = cum_stages_breakdown_expanded['Created'].max()
-# Separate date input for start date
-start_date = st.sidebar.date_input(
-    "Start Date",
-    value = min_date,
-    min_value = min_date,
-    max_value = max_date
-)
+def make_bar_chart(df):
 
-# Separate date input for end date
-end_date = st.sidebar.date_input(
-    "End Date",
-    value = max_date,
-    min_value = min_date,
-    max_value = max_date
-)
-
-# Display selected dates for debugging
-st.write(f"Period: {start_date} to {end_date}")
-
-# Ensure the start date is before the end date
-if start_date > end_date:
-    st.error("Error: Start date must be before or equal to the end date.")
-# List of potential filter columns-later
-filter_columns = ['Lead Status', 'Responsible','Source','UTM Source', 'UTM Medium', 'UTM Campaign', 'UTM Content','Nature of Project', 'D2C Website (y/n)', 'Services Needed']
-# 
-
-# Dropdown for selecting the breakdown variable
-breakdown_var = st.sidebar.selectbox(
-    'Select Breakdown Variable',
-    filter_columns
-)
-
-# Dictionary to store selected filters
-selected_filters = {}
-
-# Loop through each filter column and create multiselect dropdowns
-for col in filter_columns:
-    unique_values = list(cum_stages_breakdown_expanded[col].dropna().unique())
-    selected_values = st.sidebar.multiselect(f'Select {col}', unique_values, default=unique_values)
-    selected_filters[col] = selected_values
-
-# Filter the DataFrame based on user-selected filters and date range
-filtered_df = cum_stages_breakdown_expanded.copy()
-
-# Apply the date range filter
-filtered_df = filtered_df[(filtered_df['Created'] >= pd.to_datetime(start_date)) & 
-                          (filtered_df['Created'] <= pd.to_datetime(end_date))]
-
-# Apply additional filters for categorical columns
-for col, values in selected_filters.items():
-    filtered_df = filtered_df[filtered_df[col].isin(values)]
-
-# Group the data by Stage and the selected breakdown variable
-cumulative_grouped = filtered_df.groupby(['Stage', breakdown_var])['Count'].sum().reset_index()
-# Group the data by Stage 
-cumulative_stage = filtered_df.groupby(['Stage'])['Count'].sum().reset_index()
-
-# Create a stacked horizontal bar chart using Plotly
-fig = px.bar(
-    cumulative_grouped,
-    x='Count',
-    y='Stage',
-    color=breakdown_var,
-    orientation='h',
-    barmode='stack',
-    labels={'Count': 'Cumulative Count', 'Stage': 'Stage'},
-    title=f'Cumulative Stage Counts by {breakdown_var} (Date Range: {start_date} to {end_date})'
-)
-
-# Update layout to reverse the y-axis order
-fig.update_layout(
-    yaxis={'categoryorder': 'array', 'categoryarray': stages[::-1]},
-    xaxis_title='Cumulative Count',
-    yaxis_title='Stage',
-    legend_title=breakdown_var
-)
-
-# Add text labels for  cumulative totals
-for i, stage in enumerate(reversed(stages)):
-    try: 
-        cumulative_value = cumulative_stage[cumulative_stage['Stage'] == stage]['Count'].values[0]
-    except: 
-        cumulative_value = 0
-    fig.add_annotation(
-        x=cumulative_value,
-        y=i, # Corresponds to the y-position for each stage
-        text=str(cumulative_value),
-        showarrow=False,
-        xanchor='left',
-        yanchor='middle',
-        font=dict(size=14, color='red')
+    # Add date range filter in the sidebar
+    min_date = df['Created'].min()
+    max_date = df['Created'].max()
+    # Separate date input for start date
+    start_date = st.sidebar.date_input(
+        "Start Date",
+        value = min_date,
+        min_value = min_date,
+        max_value = max_date
     )
 
-# Display the chart
-st.plotly_chart(fig)
+    # Separate date input for end date
+    end_date = st.sidebar.date_input(
+        "End Date",
+        value = max_date,
+        min_value = min_date,
+        max_value = max_date
+    )
+
+    # Display selected dates for debugging
+    st.write(f"Period: {start_date} to {end_date}")
+
+    # Ensure the start date is before the end date
+    if start_date > end_date:
+        st.error("Error: Start date must be before or equal to the end date.")
+    # List of potential filter columns-later
+    filter_columns = ['Lead Status', 'Responsible','Source','UTM Source', 'UTM Medium', 'UTM Campaign', 'UTM Content','Nature of Project', 'D2C Website (y/n)', 'Services Needed','LS - Service Fit', 'LS - Urgency', 'LG - Budget Availability', 'LG - Decision Making Capability']
+    # 
+
+    # Dropdown for selecting the breakdown variable
+    breakdown_var = st.sidebar.selectbox(
+        'Select Breakdown Variable',
+        filter_columns
+    )
+
+    # Dictionary to store selected filters
+    selected_filters = {}
+
+    # Loop through each filter column and create multiselect dropdowns
+    for col in filter_columns:
+        unique_values = list(df[col].dropna().unique())
+        selected_values = st.sidebar.multiselect(f'Select {col}', unique_values, default=unique_values)
+        selected_filters[col] = selected_values
+
+    # Filter the DataFrame based on user-selected filters and date range
+    filtered_df = df.copy()
+
+    # Apply the date range filter
+    filtered_df = filtered_df[(filtered_df['Created'] >= pd.to_datetime(start_date)) & 
+                            (filtered_df['Created'] <= pd.to_datetime(end_date))]
+
+    # Apply additional filters for categorical columns
+    for col, values in selected_filters.items():
+        filtered_df = filtered_df[filtered_df[col].isin(values)]
+
+    # Group the data by Stage and the selected breakdown variable
+    cumulative_grouped = filtered_df.groupby(['Stage', breakdown_var])['Count'].sum().reset_index()
+    # Group the data by Stage 
+    cumulative_stage = filtered_df.groupby(['Stage'])['Count'].sum().reset_index()
+
+    # Create a stacked horizontal bar chart using Plotly
+    fig = px.bar(
+        cumulative_grouped,
+        x='Count',
+        y='Stage',
+        color=breakdown_var,
+        orientation='h',
+        barmode='stack',
+        labels={'Count': 'Cumulative Count', 'Stage': 'Stage'},
+        title=f'Cumulative Stage Counts by {breakdown_var} (Date Range: {start_date} to {end_date})'
+    )
+
+    # Update layout to reverse the y-axis order
+    fig.update_layout(
+        yaxis={'categoryorder': 'array', 'categoryarray': stages[::-1]},
+        xaxis_title='Cumulative Count',
+        yaxis_title='Stage',
+        legend_title=breakdown_var
+    )
+
+    # Add text labels for  cumulative totals
+    for i, stage in enumerate(reversed(stages)):
+        try: 
+            cumulative_value = cumulative_stage[cumulative_stage['Stage'] == stage]['Count'].values[0]
+        except: 
+            cumulative_value = 0
+        fig.add_annotation(
+            x=cumulative_value,
+            y=i, # Corresponds to the y-position for each stage
+            text=str(cumulative_value),
+            showarrow=False,
+            xanchor='left',
+            yanchor='middle',
+            font=dict(size=14, color='red')
+        )
+
+    # Display the chart
+    st.plotly_chart(fig)
+
+make_bar_chart(cum_stages_breakdown_expanded)
+make_bar_chart(non_cum_stages_breakdown_expanded)
 # Add a footer message
-st.sidebar.write("X CRM")
+st.sidebar.write("Expedify CRM")
